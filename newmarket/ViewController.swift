42be39f9c482d1d05ea9ec3bcc7cdfdc5e45ec8a
tsuka-mac-mini <tsuka-mac-mini@tsuka-mac-mini-no-mac-mini.local>
2016-06-11 10:37:55 +0900

    Initial Commit

---
+++ newmarket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newmarket
+//
+//  Created by tsuka-mac-mini on 2016/06/11.
+//  Copyright © 2016年 tsuka-mac-mini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
